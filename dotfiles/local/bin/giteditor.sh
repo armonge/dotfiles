621a662741ad1ebd63d2d447ddb561919dec780b
Andres Reyes Monge <andres.reyes@vui.agency>
2022-06-16 13:53:19 +0200

    Adding git editor from Gnome

---
+++ dotfiles/local/bin/giteditor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+
+if command -v flatpak 1>/dev/null 2>&1; then
+	flatpak run --file-forwarding re.sonny.Commit @@ $@
+elif command -v nvim 1>/dev/null 2>&1; then
+	nvim $@
+elif command -v vim 1>/dev/null 2>&1; then
+	vim $@
+fi
