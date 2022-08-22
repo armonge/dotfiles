95830d93fea469d7b2ef24c562b44e09ec185c24
Andres Reyes Monge <andres.monge@stylight.com>
2022-08-22 09:24:02 +0200

    Removes the custom git editor

--- dotfiles/local/bin/giteditor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if command -v flatpak 1>/dev/null 2>&1; then
-	flatpak run --file-forwarding re.sonny.Commit @@ $@
-elif command -v nvim 1>/dev/null 2>&1; then
-	nvim $@
-elif command -v vim 1>/dev/null 2>&1; then
-	vim $@
-fi
