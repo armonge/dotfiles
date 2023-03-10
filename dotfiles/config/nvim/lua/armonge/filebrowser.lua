dd29f2c0bccf58717008c7e43984f7088f62c624
Andres Reyes Monge <andres.monge@stylight.com>
2023-03-10 09:05:39 +0100

    Using <C-p> for telescope

--- dotfiles/config/nvim/lua/armonge/filebrowser.lua
+++ dotfiles/config/nvim/lua/armonge/filebrowser.lua
@@ -22,7 +22,7 @@ require("barbecue.ui").toggle(true)
 -- }
 -- nvim-telescope/telescope.nvim {
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fn", builtin.help_tags, {})
