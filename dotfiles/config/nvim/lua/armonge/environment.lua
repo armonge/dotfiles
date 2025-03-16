d5dffe3ac5041db25ca181e8043dccedb274effa
Andres Reyes Monge <andres.monge@parcellab.com>
2025-03-16 14:36:53 +0100

    refactor: streamline dotfiles and dependency management
    - Removed redundant actions from `config.yaml`.
    - Simplified `dotdrop.sh` by replacing `readlink`/`realpath` logic.
    - Updated Python environment setup in `environment.lua`.
    - Enhanced `install-deps.sh` by removing unused tools and adding `uv`.
    - Added `DOTDROP_VIRTUALENV` export to `bashrc`.

--- dotfiles/config/nvim/lua/armonge/environment.lua
+++ dotfiles/config/nvim/lua/armonge/environment.lua
@@ -1,14 +1,10 @@
 -- Python {
 vim.opt.pyx = 3
 
--- Python2 for neovim {
-vim.g.python2_host_prog = os.getenv("PYENV_ROOT") .. "/versions/nvim2/bin/python"
--- }
 
 -- Python3 for neovim {
-vim.g.python3_host_prog = os.getenv("PYENV_ROOT") .. "/versions/nvim3/bin/python"
-vim.g.python_host_prog = os.getenv("PYENV_ROOT") .. "/versions/nvim3/bin/python"
--- }
+vim.g.python3_host_prog = os.getenv("HOME") .. "/.config/nvim/.venv/bin/python"
+vim.g.python_host_prog = os.getenv("HOME") .. "/.config/nvim/.venv/bin/python"
 -- }
 
 -- Node {
