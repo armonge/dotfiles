a9dcc599b86b3e6cc83b2a0b9b1652a0d6923095
Andres Reyes Monge <andres.monge@parcellab.com>
2025-03-17 08:41:44 +0100

    more on wezterm

--- dotfiles/config/wezterm/config/bindings.lua
+++ dotfiles/config/wezterm/config/bindings.lua
@@ -1,16 +1,16 @@
-local wezterm = require('wezterm')
-local platform = require('utils.platform')
-local backdrops = require('utils.backdrops')
+local wezterm = require("wezterm")
+local platform = require("utils.platform")
+local backdrops = require("utils.backdrops")
 local act = wezterm.action
 
 local mod = {}
 
 if platform.is_mac then
-  mod.SUPER = 'SUPER'
-  mod.SUPER_REV = 'SUPER|CTRL'
+  mod.SUPER = "SUPER"
+  mod.SUPER_REV = "SUPER|CTRL"
 elseif platform.is_win or platform.is_linux then
-  mod.SUPER = 'ALT'  -- to not conflict with Windows key shortcuts
-  mod.SUPER_REV = 'ALT|CTRL'
+  mod.SUPER = "ALT" -- to not conflict with Windows key shortcuts
+  mod.SUPER_REV = "ALT|CTRL"
 end
 
 -- stylua: ignore
@@ -234,8 +234,8 @@ local key_tables = {
 local mouse_bindings = {
   -- Ctrl-click will open the link under the mouse cursor
   {
-    event = { Up = { streak = 1, button = 'Left' } },
-    mods = 'CTRL',
+    event = { Up = { streak = 1, button = "Left" } },
+    mods = "CTRL",
     action = act.OpenLinkAtMouseCursor,
   },
 }
@@ -243,7 +243,7 @@ local mouse_bindings = {
 return {
   disable_default_key_bindings = true,
   -- disable_default_mouse_bindings = true,
-  leader = { key = 'Space', mods = mod.SUPER_REV },
+  leader = { key = "Space", mods = mod.SUPER_REV },
   keys = keys,
   key_tables = key_tables,
   mouse_bindings = mouse_bindings,
--- dotfiles/config/wezterm/utils/backdrops.lua
+++ dotfiles/config/wezterm/utils/backdrops.lua
@@ -1,20 +1,8 @@
 local wezterm = require("wezterm")
-local colors = require("colors.custom")
-
--- Seeding random numbers before generating for use
--- Known issue with lua math library
--- see: https://stackoverflow.com/questions/20154991/generating-uniform-random-numbers-in-lua
-math.randomseed(os.time())
-math.random()
-math.random()
-math.random()
-
-local GLOB_PATTERN = "*.{jpg,jpeg,png,gif,bmp,ico,tiff,pnm,dds,tga}"
+local colors = wezterm.color.get_builtin_schemes()["Tokyo Night Storm (Gogh)"]
 
 ---@class BackDrops
 ---@field current_idx number index of current image
----@field images string[] background images
----@field images_dir string directory of background images. Default is `wezterm.config_dir .. '/backdrops/'`
 ---@field focus_color string background color when in focus mode. Default is `colors.custom.background`
 ---@field focus_on boolean focus mode on or off
 local BackDrops = {}
@@ -25,41 +13,11 @@ BackDrops.__index = BackDrops
 function BackDrops:init()
   local inital = {
     current_idx = 1,
-    images = {},
-    images_dir = wezterm.config_dir .. "/backdrops/",
     focus_color = colors.background,
     focus_on = false,
   }
   local backdrops = setmetatable(inital, self)
   return backdrops
-end
-
----Override the default `images_dir`
----Default `images_dir` is `wezterm.config_dir .. '/backdrops/'`
----
---- INFO:
----  This function must be invoked before `set_images()`
----
----@param path string directory of background images
-function BackDrops:set_images_dir(path)
-  self.images_dir = path
-  if not path:match("/$") then
-    self.images_dir = path .. "/"
-  end
-  return self
-end
-
----MUST BE RUN BEFORE ALL OTHER `BackDrops` functions
----Sets the `images` after instantiating `BackDrops`.
----
---- INFO:
----   During the initial load of the config, this function can only invoked in `wezterm.lua`.
----   WezTerm's fs utility `glob` (used in this function) works by running on a spawned child process.
----   This throws a coroutine error if the function is invoked in outside of `wezterm.lua` in the -
----   initial load of the Terminal config.
-function BackDrops:set_images()
-  self.images = wezterm.glob(self.images_dir .. GLOB_PATTERN)
-  return self
 end
 
 ---Override the default `focus_color`
@@ -75,10 +33,6 @@ end
 ---@return table
 function BackDrops:_create_opts()
   return {
-    {
-      source = { File = self.images[self.current_idx] },
-      horizontal_align = "Center",
-    },
     {
       source = { Color = colors.background },
       height = "120%",
@@ -151,65 +105,6 @@ function BackDrops:_set_focus_opt(window)
   window:set_config_overrides(opts)
 end
 
----Convert the `files` array to a table of `InputSelector` choices
----see: https://wezfurlong.org/wezterm/config/lua/keyassignment/InputSelector.html
-function BackDrops:choices()
-  local choices = {}
-  for idx, file in ipairs(self.images) do
-    table.insert(choices, {
-      id = tostring(idx),
-      label = file:match("([^/]+)$"),
-    })
-  end
-  return choices
-end
-
----Select a random background from the loaded `files`
----Pass in `Window` object to override the current window options
----@param window any? WezTerm `Window` see: https://wezfurlong.org/wezterm/config/lua/window/index.html
-function BackDrops:random(window)
-  self.current_idx = math.random(#self.images)
-
-  if window ~= nil then
-    self:_set_opt(window, self:_create_opts())
-  end
-end
-
----Cycle the loaded `files` and select the next background
----@param window any WezTerm `Window` see: https://wezfurlong.org/wezterm/config/lua/window/index.html
-function BackDrops:cycle_forward(window)
-  if self.current_idx == #self.images then
-    self.current_idx = 1
-  else
-    self.current_idx = self.current_idx + 1
-  end
-  self:_set_opt(window, self:_create_opts())
-end
-
----Cycle the loaded `files` and select the previous background
----@param window any WezTerm `Window` see: https://wezfurlong.org/wezterm/config/lua/window/index.html
-function BackDrops:cycle_back(window)
-  if self.current_idx == 1 then
-    self.current_idx = #self.images
-  else
-    self.current_idx = self.current_idx - 1
-  end
-  self:_set_opt(window, self:_create_opts())
-end
-
----Set a specific background from the `files` array
----@param window any WezTerm `Window` see: https://wezfurlong.org/wezterm/config/lua/window/index.html
----@param idx number index of the `files` array
-function BackDrops:set_img(window, idx)
-  if idx > #self.images or idx < 0 then
-    wezterm.log_error("Index out of range")
-    return
-  end
-
-  self.current_idx = idx
-  self:_set_opt(window, self:_create_opts())
-end
-
 ---Toggle the focus mode
 ---@param window any WezTerm `Window` see: https://wezfurlong.org/wezterm/config/lua/window/index.html
 function BackDrops:toggle_focus(window)
@@ -225,5 +120,3 @@ function BackDrops:toggle_focus(window)
 
   self:_set_opt(window, background_opts)
 end
-
-return BackDrops:init()
